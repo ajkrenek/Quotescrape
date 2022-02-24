cb29fdbafba0c588208bf6e501e7f83ea017ed7a
ajkrenek <98238097+ajkrenek@users.noreply.github.com>
2022-02-24 14:09:25 -0600

    Update py.py

--- py.py
+++ py.py
@@ -2,23 +2,13 @@ import re
 
 inFile = open("quotescrape.txt","r")
 text = inFile.read()
-#inFile.close()
-#pattern1 = “([^"]*)”
-#pattern2 = "([^"]*)"
-quotes = re.findall(r'''
-“(.*?)”
-
-
-
-''', text, re.VERBOSE)
+quotes = re.findall(r'“(.*?)”', text)
 outFile = open("example.txt", "a")
 with outFile as f:
     for line in quotes:
         f.write(line)
         f.write('\n')
 #print(quotes)
-
-
 
 inFile.close()
 outFile.close()
